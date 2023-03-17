d034bf4d74aa71d3d2b5a37d7959536d3b7ba916
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-17 08:48:48 -0700

    last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+include <unistd.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const int length = sizeof(message) - 1;  // exclude null terminator
+write(2, message, length);
+return (1);
+}
+
