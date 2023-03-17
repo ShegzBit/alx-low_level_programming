51d6e9275ecd062c2e9c9a1c99506fabb1966b7a
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-17 15:27:47 -0700

    2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ n = rand() - RAND_MAX / 2;
 value = n % 10;
 if (value > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, value);
+printf("Last digit of %d is %d and is greater than 5\n", n, value);
 }
 else if (value == 0)
 {
-printf("Last digit of %d is %d and is 0", n, value);
+printf("Last digit of %d is %d and is 0\n", n, value);
 }
 else if (value < 6 && value != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not zero", n, value);
+printf("Last digit of %d is %d and is less than 6 and not zero\n", n, value);
 }
 return (0);
 }
