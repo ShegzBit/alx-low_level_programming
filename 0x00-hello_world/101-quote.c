75be783b78b68c89c6bf755f0bdcba9d67c9f305
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-17 09:19:08 -0700

    me

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 const int length = sizeof(message) - 1;
 write(2, message, length);
