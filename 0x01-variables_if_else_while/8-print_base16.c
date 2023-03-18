2a267805fd8553146d82d94f13cc55ef6d7ad95e
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-17 19:05:13 -0700

    9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 16)
+{
+if (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+else
+{
+putchar(i % 10 + 'a');
+i++;
+}
+}
+putchar('\n');
+return (0);
+}
