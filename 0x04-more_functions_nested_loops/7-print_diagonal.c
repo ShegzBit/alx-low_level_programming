2b643b8f90a819b746529e77c19608cd65cb9482
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-23 18:02:43 -0700

    diadonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - Print diagonal line dependent on the integer n.
+ * @n : The number of lines using '\' characters to use
+ * Return: Void.
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
+
+if (n <= 0)
+{
+putchar('\n');
+}
+}
