675156bf7e16a2071ff9012a02d6f427db0e6951
Shegzbit <feranmiawotubo16@gmail.com>
2023-03-27 12:32:16 -0700

    and the first shall be the last and the last shall be the first and all other shall follow in accordance

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints reverse of a string
+ * @s : string to reverse
+ */
+void print_rev(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+while (i >= 0)
+{
+putchar(s[i]);
+i--;
+}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void print_rev(char *s);
 void _puts(char *str);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
