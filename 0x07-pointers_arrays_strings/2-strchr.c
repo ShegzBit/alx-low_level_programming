787ca9f67dae05ce040313a2bc8a213b5f47353f
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-03 21:14:05 +0100

    added a character locator

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - finds the first characterin the string passed to it
+ * @s : string to be passed
+ * @c : character to find
+ * Return: pointer to the first character found
+ */
+char *_strchr(char *s, char c)
+{
+int i, length;
+char *ptr = NULL;
+for (length = 0; s[length] != 0; length++)
+{
+}
+for (i = 0; i < length; i++)
+{
+if (s[i] == c)
+{
+ptr = (s + i);
+}
+}
+return (ptr);
+}
