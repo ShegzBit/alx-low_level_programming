a2f632b2a3a65f94f9caf93965ac37055faf21e2
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-05 20:20:05 +0100

    added a correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
+#include <strlen>
 /**
  * _print_rev_recursion - prints a function from back to front
  * @s : function to print
  */
 void _print_rev_recursion(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-}
+int i = strlen(s);
 if (i == 0)
 {
 putchar('\n');
 }
 else
 {
---i;
+i -=1;
 putchar(s[i]);
 s[i] = '\0';
 _print_rev_recursion(s);
