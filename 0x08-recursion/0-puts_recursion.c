388ce66be2364f477a949808dab0fa83bce1ace5
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-05 18:45:55 +0100

    added puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a null terminated string
+ * @s - string to be inserted
+ */
+void _puts_recursion(char *s)
+{
+putchar(*s);
+s++;
+if (*s != '\0')
+{
+_puts_recursion(s);
+}
+else
+{
+putchar('\n');
+}
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+A repo for leaning recursion in strings
