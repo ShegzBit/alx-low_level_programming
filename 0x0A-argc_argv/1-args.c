33345f4d5383f6ad21483d7a11fdd2163aa79030
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-07 07:32:52 +0100

    added a argument count printing program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of var passed to it
+ * @argc : argument count
+ * @argv : array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
