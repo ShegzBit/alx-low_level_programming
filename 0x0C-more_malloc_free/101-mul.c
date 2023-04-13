ae5e76f75fba371ab196cff9b6f9c8933cc9d684
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-14 00:30:40 +0100

    added solutions

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * main - entry point
+ * @ac : args count
+ * @av : args vector
+ * Return: 0 on success
+ */
+int main(int ac, char *av[])
+{
+unsigned int max = ac - 1, i, j;
+int first, second, mul;
+if (max != 2)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 1; i < max; i++)
+{
+for (j = 0; j < strlen(av[i]); j++)
+{
+if (!isdigit(av[i][j]))
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+first = atoi(av[1]);
+second = atoi(av[2]);
+mul = first *second;
+printf("%d\n", mul);
+return (0);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
