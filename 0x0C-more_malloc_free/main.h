582a5a02f17356d7eb4a651cb30fb5fb3b1c6906
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-13 23:12:11 +0100

    added calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
