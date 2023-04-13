a72304dbb9b289e3e584c8b6ba3317a5a0e32149
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-13 23:17:27 +0100

    added array range

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
