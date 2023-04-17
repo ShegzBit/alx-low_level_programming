e11cebf3a3aa87940b9a52170bbe18783e1c9e9a
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-17 17:18:54 +0100

    added correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,10 +7,9 @@
  * @age : dog's age
  * @owner : dog owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (!(d == NULL))
+if (d != NULL)
 {
 d->name = name;
 d->age = age;
