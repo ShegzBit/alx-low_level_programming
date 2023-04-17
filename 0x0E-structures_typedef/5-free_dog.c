f37fce55d4bc2a4dae44ea9742cbf3f8e62df63e
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-18 00:17:22 +0100

    added a correction

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,17 @@
-#include <stddef.h>
 #include <stdlib.h>
-#include "main.h"
+#include "dog.h"
+
 /**
- * free_dog - frees dog
- * @dog_t : dog_t structure
+ * free_dog - free the stubborn dog
+ * @d: the stubborn dog
+ *
  */
 void free_dog(dog_t *d)
 {
 if (d != NULL)
 {
+free(d->name);
 free(d->owner);
 free(d);
-free(d->name);
 }
 }
