8aa359eb9a93947d4e8881298c4f10320f8cd2f5
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-17 15:09:22 +0100

    added correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - initializes dog
@@ -9,7 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (!(d == NULL))
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
