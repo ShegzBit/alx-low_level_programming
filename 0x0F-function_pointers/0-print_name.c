2c6bbb68bae73648bdc32c25bc5ac4a0ecf466ba
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-19 20:19:51 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,12 @@
 /**
  * print_name - calls the function passed to it
  * @name : name of person to be printed
- * @f() : function to be called
+ * @f : function to be called
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void(*f)(char *))
+{
+if (f != NULL)
 {
 f(name);
+}
 }
