b6bff07fe53921e6a6dbfb57358b40eff880e70e
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-20 15:07:20 +0100

    added correction to print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,17 +11,23 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-  unsigned int i;
-  va_list ap;
-  va_start(ap, n);
-  for (i = 0; i < n; i++)
-    {
-      printf("%d", va_arg(ap, int));
-      if (i != (n - 1))
-	{
-	  printf("%s", separator);
-	}
-    }
-  va_end(ap);
-  putchar('\n');
+unsigned int i;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
+if (i < (n - 1))
+{
+if (separtor == NULL)
+{
+}
+else
+{
+printf("%s", separator);
+}
+}
+}
+va_end(ap);
+putchar('\n');
 }
