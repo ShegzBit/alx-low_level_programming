8e9443bc855e10625c65e9c74a5b28e1e497241d
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-20 17:43:28 +0100

    added a solution to print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_
 #define HEADER_
+void print_all(const char * const format, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
