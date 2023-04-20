297aea15781917e79bb09d2cb1977cb64d8d6279
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-20 15:34:11 +0100

    added solution to print string

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_
 #define HEADER_
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 
 #endif
