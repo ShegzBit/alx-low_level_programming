0b31c74d2f0e9c03207e0b95ca5491f61a938e23
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-20 14:48:12 +0100

    added a solution to print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers passed to it
+ * @separator : string to print between the numbers
+ * @n : number of numbers
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  unsigned int i;
+  va_list ap;
+  va_start(ap, n);
+  for (i = 0; i < n; i++)
+    {
+      printf("%d", va_arg(ap, int));
+      if (i != (n - 1))
+	{
+	  printf("%s", separator);
+	}
+    }
+  va_end(ap);
+  putchar('\n');
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_
 #define HEADER_
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 
 #endif
