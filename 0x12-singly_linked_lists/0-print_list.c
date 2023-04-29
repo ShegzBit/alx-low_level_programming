10d0b001ab93472baaa1a67b742e246f67408fea
ShegzBit <feranmiawotubo16@gmail.com>
2023-04-29 15:33:30 +0100

    adding solution to task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - prints the nodes of a list
+ * @h : pointer to start of a list
+ * Return: length of list
+ */
+size_t print_list(list_t *h)
+{
+unsigned int i = 1;
+if (h != NULL)
+{
+if (h->str != NULL)
+{
+printf("%c%d%c %s\n", '[', h->len, ']', h->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+h = h->next;
+i++;
+print_list(h);
+}
+return (i);
+}
