c3d6cd985dfdb00fe6ab5a57126751ac3224f69f
ShegzBit <107847387+ShegzBit@users.noreply.github.com>
2023-05-04 06:23:12 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,5 +0,0 @@
-#ifndef OBJ_LIKE_MACRO
-#define OBJ_LIKE_MACRO
-#define SIZE 1024
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNC_LIKE_H
-#define FUNC_LIKE_H
-
-#define ABS(x) ((x < 0) ? (-(x)) : (x))
-
-#endif
