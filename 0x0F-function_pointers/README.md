f6f707d85e4c9ea143325e4546f13324c367dbbf
ShegzBit <107847387+ShegzBit@users.noreply.github.com>
2023-05-04 06:24:10 +0100

    Deleted for Alx Deferment

--- 0x0F-function_pointers/README.md
+++
@@ -1,1 +0,0 @@
-A directory for practiceof ```pointers to functions``` ```void (*add)(int, int);```
--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEADER
-#define HEADER
-int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));
-void print_name(char *name, void (*f)(char *));
-
-#endif
