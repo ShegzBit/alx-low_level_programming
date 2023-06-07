c11031ddd223d06fb4c7f6500bbee809e2e82072
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-07 18:56:37 +0100

    solved factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,23 @@
 #include "main.h"
+
+/**
+ * factorial - calculates factorial
+ * @n: number to solve for
+ * Return: n
+*/
 int factorial(int n)
 {
-    if(n == 0)
-    {
-        return(1);
-    }
-    else if (n > 0)
-    {
-        n *= factorial(n - 1);
-    }
-    else if (n < 0)
-    {
-        return (-1);
-    }
-    return (n);
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 0)
+	{
+		n *= factorial(n - 1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n);
 }
