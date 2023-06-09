53af2839ce9b847b618745445bd01c6426ed43cf
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-09 21:14:51 +0100

    added contemporary solution

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,82 +1,37 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
- * _strlen - checks length of str s
- * @s: string to check
- * Return: num -> strig length
-*/
-int _strlen(char *s)
-{
-	int num = 0;
-
-	while (*s != '\0')
-	{
-		num++;
-		s++;
-	}
-	return (num);
-}
-
-/**
- * _isdigit - checks if a character is a digit
- * @c: char to check
- * Return: 1 on true
-*/
-int _isdigit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * isnumeral - checks if a str contains only digits
- * @s: str to work on
- * Return: 1 on true
-*/
-int isnumeral(char *s)
-{
-	int n, size = _strlen(s);
-
-	while (size--)
-	{
-		n = _isdigit(*s);
-		if (n == 0)
-		{
-			return (0);
-		}
-		s++;
-	}
-	return (1);
-}
-
-
-/**
- * main - adds to infinity
- * @ac: args count
- * @av: args vector
+ * main - print sum of integer arguments passed to it
+ * @argc : argument count
+ * @argv : array of arguments
  * Return: 0 on success
-*/
+ */
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int sum = 0, i = 1;
-
-	if (ac == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	while (i < ac)
-	{
-		if (isnumeral(av[i]))
-		{
-			sum += atoi(av[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (-1);
-		}
-		i++;
-	}
-	printf("%d\n", sum);
+int mul = 0, i, j, size;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+size = strlen(argv[i]);
+for (j = 0; j < size; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+mul += atoi(argv[i]);
+}
+printf("%d\n", mul);
+}
+return (0);
 }
