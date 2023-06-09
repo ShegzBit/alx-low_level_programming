8ffad73f7e9762f9c0669342bef9e0fe008114a8
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-09 12:43:29 +0100

    program that prints arguments to screen

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * main - prints all arguments
+ * @ac: args count
+ * @av: args vector
+ * Return: 0 on success
+*/
+int main(int ac, char **av)
+{
+	while (ac--)
+	{
+		printf("%s\n", *av++);
+	}
+	return (0);
+}
