a536de2da8964d9c2dae8ac9e5d154c76b39bd79
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-13 17:00:30 +0100

    added a correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,39 @@
 #include "main.h"
 
+
+/**
+ * strcon_helper - helps _str_concat
+ * @s1: destination
+ * @s2: src
+ * Return: ret = s1 + s2 + '\0'
+*/
+char *strcon_helper(char *s1, char *s2)
+{
+	int n1, n2, size, i = 0, j = 0;
+	char *ret;
+
+	n1 = strlen(s1), n2 = strlen(s2), size = n1 + n2 + 1;
+	ret = malloc(size * sizeof(char));
+	i = size - 1;
+	if (ret == NULL)
+	return (NULL);
+
+	while (i < n1)
+	{
+		ret[i] = s1[i];
+		i++;
+	}
+	while (i < size)
+	{
+		ret[i] = s2[j];
+		i++;
+		j++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+
 /**
  * str_concat - string concatenator
  * @s1: string 1 to concatenate
@@ -8,37 +42,16 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int n1, n2, size , i = 0, j = 0;
+	int n1, n2, i;
 	char *ret;
-	
+
 	if (s1 != NULL && s2 != NULL)
 	{
-		n1 = strlen(s1);
-		n2 = strlen(s2);
-		size = n1 + n2;
-		ret = malloc(size * sizeof(char));
-
-		if (ret == NULL)
-		return (NULL);
-
-		while (i < n1)
-		{
-			ret[i] = s1[i];
-			i++;
-		}
-
-		while (i < size)
-		{
-			ret[i] = s2[j];
-			i++;
-			j++;
-		}
-		ret[i] = '\0';
+		ret = strcon_helper(s1, s2);
 	}
 	else if (s1 == NULL && s2 != NULL)
 	{
-		n1 = 0;
-		n2 = strlen(s2);
+		n1 = 0, n2 = strlen(s2) + 1, i = n2 - 1;
 		ret = malloc(n2 * sizeof(char));
 
 		if (ret == NULL)
@@ -46,17 +59,22 @@ char *str_concat(char *s1, char *s2)
 
 		strcpy(ret, s2);
 	}
-
 	else if (s1 != NULL && s2 == NULL)
 	{
-		n2 = 0;
-		n1 = strlen(s1);
+		n2 = 0, n1 = strlen(s1) + 1, i = n1 - 1;
 		ret = malloc(n1 * sizeof(char));
 
 		if (ret == NULL)
 		return (NULL);
 
 		strcpy(ret, s1);
+		ret[i] = '\0';
 	}
+	else
+	{
+		ret = malloc(1 * sizeof(char));
+		ret[0] = '\0';
+	}
+	
 	return (ret);
 }
