3307ba8e884323702a4c011ffec941669c90cb74
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-13 15:08:05 +0100

    modified task 2 to suit requirement based on str case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,23 +8,55 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int n1 = strlen(s1), n2 = strlen(s2), size = n1 + n2, i = 0, j = 0;
-	char *ret = malloc(size * sizeof(char));
-
-	if (ret == NULL)
-	return (NULL);
-
-	while (i < n1)
+	int n1, n2, size , i = 0, j = 0;
+	char *ret;
+	
+	if (s1 != NULL && s2 != NULL)
 	{
-		ret[i] = s1[i];
-		i++;
+		n1 = strlen(s1);
+		n2 = strlen(s2);
+		size = n1 + n2;
+		ret = malloc(size * sizeof(char));
+
+		if (ret == NULL)
+		return (NULL);
+
+		while (i < n1)
+		{
+			ret[i] = s1[i];
+			i++;
+		}
+
+		while (i < size)
+		{
+			ret[i] = s2[j];
+			i++;
+			j++;
+		}
+		ret[i] = '\0';
+	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		n1 = 0;
+		n2 = strlen(s2);
+		ret = malloc(n2 * sizeof(char));
+
+		if (ret == NULL)
+		return (NULL);
+
+		strcpy(ret, s2);
 	}
 
-	while (i < size)
+	else if (s1 != NULL && s2 == NULL)
 	{
-		ret[i] = s2[j];
-		i++;
-		j++;
+		n2 = 0;
+		n1 = strlen(s1);
+		ret = malloc(n1 * sizeof(char));
+
+		if (ret == NULL)
+		return (NULL);
+
+		strcpy(ret, s1);
 	}
 	return (ret);
 }
