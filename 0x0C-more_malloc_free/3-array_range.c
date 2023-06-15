93f9c61028370ae4908724f50d9f67fe48fec442
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-15 22:25:47 +0100

    added correction to task 3 array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,16 +8,18 @@
 */
 int *array_range(int min, int max)
 {
-	int size = max - min + 1, i = 0;
-	int *arr = malloc(size * sizeof(int));
-
-	if (arr == NULL)
-		return (NULL);
+	int *arr;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	while (i < size)
-		arr[i++] = min++;
+
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		arr[i] = min;
 
 	return (arr);
 }
