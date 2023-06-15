4a249148aa4e5a726aeff4ae2b0cbb6d8203e1c0
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-15 23:21:16 +0100

    added task 4 solution realloc

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -1,54 +1,70 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    char *p;
-    int i;
+    char *str;
+    int *arr;
+    unsigned int old_size, new_size, i;
 
-    p = malloc(sizeof(char) * 10);
-    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-    i = 0;
-    while (i < 98)
-    {
-        p[i++] = 98;
-    }
-    simple_print_buffer(p, 98);
-    free(p);
-    return (0);
+    /* Test 1: Reallocate string to larger size */
+    str = malloc(10);
+    strcpy(str, "Holberton");
+    printf("Test 1 - Old string: %s\n", str);
+
+    old_size = 10;
+    new_size = 15;
+    str = _realloc(str, old_size, new_size);
+    printf("Test 1 - New string: %s\n", str);
+
+    free(str);
+
+    /* Test 2: Reallocate string to smaller size */
+    str = malloc(10);
+    strcpy(str, "Holberton");
+    printf("Test 2 - Old string: %s\n", str);
+
+    old_size = 10;
+    new_size = 5;
+    str = _realloc(str, old_size, new_size);
+    printf("Test 2 - New string: %s\n", str);
+
+    free(str);
+
+    /* Test 3: Reallocate integer array to larger size */
+    arr = malloc(5 * sizeof(int));
+    arr[0] = 98;
+    arr[1] = 402;
+    arr[2] = -198;
+    arr[3] = 298;
+    arr[4] = -1024;
+
+    printf("Test 3 - Old array: ");
+    for (i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    old_size = 5 * sizeof(int);
+    new_size = 10 * sizeof(int);
+    arr = _realloc(arr, old_size, new_size);
+
+    printf("Test 3 - New array: ");
+    for (i = 0; i < 10; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    free(arr);
+
+    /* Test 4: Reallocate NULL pointer */
+    str = NULL;
+    printf("Test 4 - Old string: %s\n", str);
+
+    old_size = 0;
+    new_size = 10;
+    str = _realloc(str, old_size, new_size);
+    printf("Test 4 - New string: %s\n", str);
+
+    free(str);
+
+   return (0);
 }
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,30 +9,28 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	/**
- * if new size = old size rturn ptr
- * if ptr = null alloc newsize
- * if ptr != null and new size is zero free ptr return NULL
- * if new size > old size copy all old into new
-*/
-	void *ret;
-	int size;
+    char *new_ptr;
+    unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-		ret = malloc(new_size);
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	size = (new_size < old_size) ? new_size : old_size;
-	ret = malloc(new_size);
-	if (ret == NULL)
-		return (NULL);
-	memcpy(ret, ptr, size);
-	free(ptr);
+    if (new_size == old_size)
+        return (ptr);
 
-	return (ret);
+    if (ptr == NULL)
+        return (malloc(new_size));
+
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+        return (NULL);
+
+    for (i = 0; i < old_size && i < new_size; i++)
+        new_ptr[i] = ((char *)ptr)[i];
+
+    free(ptr);
+    return (new_ptr);
 }
