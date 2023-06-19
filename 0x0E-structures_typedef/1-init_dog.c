fa68f32798d2a7d7274cbb827515cfa093f36dcd
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-19 05:08:05 +0100

    added solution to task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function to init dog
+ * @d: dog to init
+ * @name: d name
+ * @age: d age
+ * @owner: d owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
