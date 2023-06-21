5cf6635fa1578ad67788e1638ef022d1a1d4170a
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-21 17:58:45 +0100

    check codes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+
+/**
+ * main - uses all othe 3- functions
+ *
+ * Return: 0 in success
+ */
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
