12bbf0d3f50b9adea624bf6ca37af73c9a72fe73
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-21 12:15:08 +0100

    added solution task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+
+/**
+ * int_index - compares
+ * @array: array to work on
+ * @size: size of array
+ * @cmp: function to use
+ * Return: index of element not compared true
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		if (cmp(array[i++]) != 0)
+			return (i);
+	}
+	return (-1);
+}
