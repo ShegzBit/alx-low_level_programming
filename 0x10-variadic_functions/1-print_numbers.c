c7b70128dab813e42fff2b37ce8e361c5ef7075f
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-22 17:28:54 +0100

    added correction to task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
+		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
 		i++;
 	}
