29ba45d5a8b7d4d7001fb93275d6b7ec23f9d676
ShegzBit <feranmiawotubo16@gmail.com>
2023-06-22 23:44:06 +0100

    added a solution to task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints all datatype
+ * @format: format for print handling
+ */
+void print_all(const char * const format, ...)
+{
+va_list ap;
+unsigned int j = 0, i = 0;
+char *str;
+
+va_start(ap, format);
+while (format && format[j] != '\0')
+{
+switch (format[j])
+{ case 'c':
+switch (i)
+{ case 1: printf(", "); }
+i = 1;
+printf("%c", va_arg(ap, int));
+break;
+case 'i':
+switch (i)
+{ case 1: printf(", "); }
+i = 1;
+printf("%i", va_arg(ap, int));
+break;
+case 'f':
+switch (i)
+{ case 1: printf(", "); }
+i = 1;
+printf("%f", va_arg(ap, double));
+break;
+case's':
+switch (i)
+{ case 1: printf(", "); }
+i = 1;
+str = va_arg(ap, char*);
+if (str)
+{ printf("%s", str);
+break; }
+printf("%p", str);
+break; }
+j++;
+}
+printf("\n");
+va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,11 +5,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <string.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_i(va_list ap);
+void print_s(va_list ap);
+void print_c(va_list ap);
+void print_f(va_list ap);
+
+typedef void (*prn)(va_list);
+
+/**
+ * struct op_t - struct op_t
+ * @op: char
+ * @type: type to handle with
+ */
+typedef struct op_t
+{
+	char op;
+	prn func;
+
+} op_t;
 
 
 #endif
