38c9ca91a5f1970db49533cc49fa7258bed70d43
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-05 14:34:53 +0100

    added solution to task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint list
+ * @head: head of list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	/*Loop while head is not null and free all spaces alloced through malloc*/
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		(*head) = temp;
+	}
+	head = NULL;
+}
