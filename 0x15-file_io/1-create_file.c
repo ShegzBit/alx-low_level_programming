c098e03515e82db8485ff11f3728948dc3364e3a
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-11 18:40:25 +0100

    added potential solution task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,29 +8,27 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	/*Create fd and other neccessary variables*/
+/*Create file descriptor*/
 	int fd, written;
 
-	/*Handle for txt content or filename is null*/
 	if (filename == NULL)
 		return (-1);
-
 	text_content = (text_content == NULL) ? "" : text_content;
-
-	/*Create file if it doesn't exist and make permission rw*/
-	fd = open(filename, O_WRONLY | O_TRUNC);
+/*Open fd*/
+	fd = open(filename, O_WRONLY | O_CREAT);
 	if (fd == -1)
-		open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
-	/*Write to file and close file*/
-	written = write(fd, (void *)text_content, strlen(text_content));
-	if (written == -1)
+		fd = open(filename, O_WRONLY);
+	else
 	{
-		close(fd);
-		return (-1);
+		fd = open(filename, O_WRONLY | O_TRUNC);
+		if (fd == -1)
+			return (-1);
 	}
-
+/*write to it*/
+	written = write(fd, text_content, strlen(text_content));
+	if (written == -1)
+		return (-1);
+	/*Close fd*/
+	close(fd);
 	return (1);
 }
