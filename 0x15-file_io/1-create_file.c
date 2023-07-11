2c3e054becd6488307e4b146b121533c5f335aab
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-11 18:48:38 +0100

    added potential solution task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	text_content = (text_content == NULL) ? "" : text_content;
 /*Open fd*/
-	fd = open(filename, O_WRONLY | O_CREAT);
+	fd = open(filename, O_WRONLY);
 	if (fd == -1)
-		fd = open(filename, O_WRONLY);
+		fd = open(filename, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
 	if (fd == -1)
 		return (-1);
 /*write to it*/
