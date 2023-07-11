c1e917e2eb4b5b35a05e6fb53256636b8376f1be
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-11 02:51:26 +0100

    added a potential solution to task2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	text_content = (text_content == NULL) ? "" : text_content;
 /*Open fd*/
-	fd = open(filename, O_WRONLY | O_CREAT);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		fd = open(filename, O_WRONLY);
 	else
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: file name
+ * @text_content: content to be written
+ * Return: 1 on success | -1 on failure
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-    /*Create file descriptors and neccessary variables*/
-    int fd, written;
+/*Create file descriptors and neccessary variables*/
+	int fd, written;
 
-    /*Check if filename is null*/
-    if (filename == NULL)
-        return (NULL);
-    /*Handle for text_content is null*/
-    text_content = (text_content == NULL) ? "" : text_content;
-    /*if the file does not exist return -1*/
+/*Check if filename is null*/
+	if (filename == NULL)
+		return (-1);
+/*Handle for text_content is null*/
+	text_content = (text_content == NULL) ? "" : text_content;
+/*if the file does not exist return -1*/
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	written = dprintf(fd, "%s", text_content);
+
+	if (written == -1)
+		return (-1);
+	return (1);
 }
