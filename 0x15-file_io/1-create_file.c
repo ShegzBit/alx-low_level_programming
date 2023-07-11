04b021ac6415f9917c46bc7e2d0accfc2f595693
ShegzBit <feranmiawotubo16@gmail.com>
2023-07-11 02:15:29 +0100

    added potential correction to task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_WRONLY);
-		return (1);
-	}
+	text_content = (text_content == NULL) ? "" : text_content;
 /*Open fd*/
 	fd = open(filename, O_WRONLY | O_CREAT);
 	if (fd == -1)
