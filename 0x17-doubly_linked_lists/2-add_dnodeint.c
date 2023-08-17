a331b9b1db2e23e7b5e76d25d32d7300eda06aa7
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-17 21:03:31 +0100

    Solved task 2 add node int to beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new data to the beginning of a list
+ * @head: head of list to add data to
+ * @n: data to add to list
+ * Return: pointer to new node being added
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(dlistint_t));
+	temp->n = n;
+	temp->next = *head;
+	temp->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = temp;
+	*head = temp;
+	return (temp);
+
+}
