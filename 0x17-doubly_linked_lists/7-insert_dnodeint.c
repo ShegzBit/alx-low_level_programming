0f07368f499353bb6f6ea140a4106c6c8bb952b1
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-18 01:54:06 +0100

    Check alloc success and fix index not insertable

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (temp == NULL || idx == 0)
+	if (idx == 0)
 	{
 		new_node->next = temp;
 		new_node->prev = NULL;
