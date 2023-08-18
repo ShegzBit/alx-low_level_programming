267f11112afb2e174494af02957be8c38d20a2a1
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-18 01:34:31 +0100

    Solved task 7 insert node at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts an element at index idx
+ * @h: pointer to head node of list
+ * @idx: index to insert n into
+ * @n: data to insert
+ * Return: Address of new node | NULL on alloc fail
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h, *new_node, *tmp_prev;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	if (temp == NULL || idx == 0)
+	{
+		new_node->next = temp;
+		new_node->prev = NULL;
+		(*h) = new_node;
+		return (new_node);
+	}
+
+	for (i = 0; i < idx && temp != NULL; temp = temp->next, i++)
+		tmp_prev = temp;
+
+	new_node->next = temp;
+	new_node->prev = tmp_prev;
+	if (tmp_prev != NULL)
+		tmp_prev->next = new_node;
+	if (temp != NULL)
+		temp->prev = new_node;
+	return (new_node);
+}
---
+++ 0x17-doubly_linked_lists/7-main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+
+    printf("/*Insert in between*/\n");
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+
+    printf("/*Insert at beginning*/\n");
+     printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 0, 4);
+    print_dlistint(head);
+
+    printf("/*Insert at second to last*/\n");
+     printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 9, 4096);
+    print_dlistint(head);
+
+    printf("/*Insert at end*/\n");
+     printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 11, 13);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
