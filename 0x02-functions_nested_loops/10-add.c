febd0e798b9013054e66b055c61381b92971c469
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-02 12:28:15 +0100

    Solved task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - takes the sum of two numbers
+ * @a: first number to add
+ * @b:second number to add
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,6 @@ void print_alphabet_x10(void);
 void print_alphabet(void);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 
 #endif /*_MAIN_H_*/
