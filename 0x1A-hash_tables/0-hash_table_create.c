0e4294ad1cd12c03747af8652cd022cc1b845285
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-08 00:09:16 +0100

    SOlved task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - creates a new hash table
+ * @size: size of the table
+ * Return: new hash table | NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hs_table;
+
+	hs_table = malloc(sizeof(hash_table_t));
+	if (hs_table == NULL)
+		return (NULL);
+	hs_table->size = size;
+	hs_table->array = calloc(hs_table->size, sizeof(hash_node_t));
+	if (hs_table->array == NULL)
+		return (NULL);
+	return (hs_table);
+}
