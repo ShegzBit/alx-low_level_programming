b0ef90d150291151f21a264ba386dbf9e7748a75
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-08 00:34:16 +0100

    solved task 1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,6 @@ typedef struct hash_table_s
 
 /*Hash Table operation functions*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /*_HASH_TABLES_H_*/
