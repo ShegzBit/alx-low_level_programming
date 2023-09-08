e6c2e1fa3211a8023e67a2a5cae90642fad6554c
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-08 21:02:34 +0100

    Solved task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,17 @@
 #include "hash_tables.h"
 
+
+/**
+ * hash_table_set - sets a value in the hash table
+ * @ht: table to set in
+ * @key: key of the pair
+ * @value: value in pair to set
+ * Return: 1 on success | 0 on failure
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
-	unsigned long int ht_index;
+	hash_node_t *node, *cur_hs = NULL;
+	unsigned long int index;
 
 	if (strlen(key) == 0)
 		return (0);
@@ -11,9 +19,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (node == NULL)
 		return (0);
 	node->next = NULL;
-	node->key = key;
-	strdup(node->value, value);
+	node->key = (void *)key;
+	node->value = strdup(value);
 	if (node->value == NULL)
 		return (0);
-	ht_index = key_index(
+	index = key_index((void *)key, ht->size);
+	cur_hs = ht->array[index];
+	if (cur_hs == NULL)
+		ht->array[index] = node;
+	else
+	{
+		if (strcmp(cur_hs->key, key) == 0)
+		{
+			free(ht->array[index]);
+			ht->array[index] = node;
+		}
+		else
+		{
+			node->next = cur_hs->next;
+			cur_hs->next = node;
+		}
+	}
+	return (1);
+
 }
---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+    const unsigned char *key = "betty", *key2 = "hetairas";
+    int index = key_index(key, 1024);
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "hetairas", "unknown");
+    hash_table_set(ht, "mentioner", "cool");
+    printf("%s\n", ht->array[index]->value);
+    index = key_index(key2, 1024);
+    printf("%s\n", ht->array[index]->next->value);
+    return (EXIT_SUCCESS);
+}
