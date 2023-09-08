7671cec5e606a53841be53b7882694759d3fa2f8
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-08 23:53:11 +0100

    fixed betty error in .h file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-int handle_collision(hash_table_t *ht, unsigned long int index, hash_node_t *node);
+int handle_collision(hash_table_t *ht, unsigned long int index,
+		hash_node_t *node);
 
 #endif /*_HASH_TABLES_H_*/
