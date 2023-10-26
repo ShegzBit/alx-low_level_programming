e1d0060031b0efc524af7c526cb21c5491935d75
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-26 02:18:58 +0100

    Solved task 5 <2>

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,23 +12,29 @@ def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid
     """
+    # tracks breadth if 1 appears more than once
+    # resets to 0 after each row
     is_b = 0
     length = 0
     breadth = 0
+    # list of breadths
+    list_b = []
+
     for arr in grid:
-        # tracks a breadth reset to 0 after each row
-        # based on if there is more than one 1 in a row
+        breadth = 0
         is_b = 0
         if 1 in arr:
             length += 1
-            for obj in arr:
-                # check if obj is 1 and is_b is 1
-                if obj == 1 and is_b == 1:
+            for cell in arr:
+                if cell == 1 and is_b == 1:
                     breadth += 1
-                    # set is_b to 1 after every encounter of 1
-                if obj == 1:
+                if cell == 1:
                     is_b = 1
-    if breadth == 0:
-        return 0
-    perimeter = 2 * (length + (breadth + 1))
-    return perimeter
+            if breadth > 0:
+                list_b.append(breadth + 1)
+    if len(list_b) == 0:
+        return
+    sum_b = 0
+    for breadth in list_b:
+        sum_b += breadth
+    return 2 * (sum_b + length)
