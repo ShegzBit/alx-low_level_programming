d7b42f40f6af4aeb80903e32d02490478793abe2
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-12 19:29:22 +0100

    solved task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+
+/**
+ * jump_search - search a jump search operation on
+ * @array: of size
+ * @size: of the value
+ * @value: and
+ * Return: the index of first occurence of value or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t start, stop;
+	int step = sqrt(size);
+
+	start = 0;
+	stop = 0;
+	if (!array || !size)
+		return (-1);
+	while (stop < size)
+	{
+		if (array[stop] >= value)
+		{
+			break;
+		}
+		start = stop;
+		stop += step;
+		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", start, stop);
+	if (stop > size)
+		stop = size - 1;
+	while (start <= stop)
+	{
+		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+	}
+	return (-1);
+}
