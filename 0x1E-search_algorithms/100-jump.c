c459afce54a6b95f213a48faa62f659dc22f25e6
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-12 20:33:34 +0100

    solvd task 100 with my sqrt implementation

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,20 @@
 #include "search_algos.h"
 
+/**
+ * _sqrt - finds the squareroot of a number
+ * @num: number of whose squreroot to find
+ * Return: the sqrt of number
+ */
+int _sqrt(int num) {
+    double guess = num / 2.0;
+    double epsilon = 0.00001;
+
+    while ((guess * guess - num) >= epsilon || (num - guess * guess) >= epsilon) {
+        guess = (guess + num / guess) / 2.0;
+    }
+
+    return guess;
+}
 
 /**
  * jump_search - search a jump search operation on
@@ -11,14 +26,17 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t start, stop;
-	int step = sqrt(size);
+	int step = _sqrt(size);
 
 	start = 0;
 	stop = 0;
+	/*Handle for null or 0 sized array*/
 	if (!array || !size)
 		return (-1);
+	/*Jump until the end of the loop*/
 	while (stop < size)
 	{
+		/*Break if value boundary has been crossed*/
 		if (array[stop] >= value)
 		{
 			break;
@@ -29,8 +47,10 @@ int jump_search(int *array, size_t size, int value)
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n", start, stop);
+	/*If stop took some steps ahead of the array return to the original size*/
 	if (stop > size)
 		stop = size - 1;
+	/*Loop through boundary to find the value*/
 	while (start <= stop)
 	{
 		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
