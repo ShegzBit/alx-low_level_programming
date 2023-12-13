c9a5b490f054a43ddde40201a00c82dc6c5b58a7
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-13 02:26:39 +0100

    added prototype to headerfile

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,11 @@
 #define __SEARCH_ALGOS__
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int search(int *array, size_t low, size_t high, int value, size_t size);
 #endif /*__SEARCH_ALGOS__*/
